b0ff091ac14f97e20496dfb8f120c84564a9fc80
Zed A. Shaw <zedshaw@zedshaw.com>
2011-09-05 13:06:34 -0700

    Doubles don't need f at the end.

--- code/ex6.c
+++ code/ex6.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
     int distance = 100;
     float power = 2.345f;
-    double super_power = 56789.4532f;
+    double super_power = 56789.4532;
     char initial = 'A';
     char first_name[] = "Zed";
     char last_name[] = "Shaw";
--- code/ex7.c
+++ code/ex7.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
     int bugs = 100;
-    double bug_rate = 1.2f;
+    double bug_rate = 1.2;
 
     printf("You have %d bugs imaginary rate of %f.\n",
             bugs, bug_rate);
