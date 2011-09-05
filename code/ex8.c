00d196c2a6ae795017e226419dc1c2d089405781
Zed A. Shaw <zedshaw@zedshaw.com>
2011-09-05 03:03:04 -0700

    Format the code to fit on the page.

--- code/ex8.c
+++ code/ex8.c
@@ -11,14 +11,16 @@ int main(int argc, char *argv[])
     };
 
     printf("The size of an int: %ld\n", sizeof(int));
-    printf("The size of areas (int[]): %ld\n", sizeof(areas));
+    printf("The size of areas (int[]): %ld\n",
+            sizeof(areas));
     printf("The number of ints in areas: %ld\n", 
             sizeof(areas) / sizeof(int));
     printf("The first area is %d, then 2nd %d.\n",
             areas[0], areas[1]);
 
     printf("The size of a char: %ld\n", sizeof(char));
-    printf("The size of name (char[]): %ld\n", sizeof(name));
+    printf("The size of name (char[]): %ld\n",
+            sizeof(name));
     printf("The number of chars: %ld\n",
             sizeof(name) / sizeof(char));
 
