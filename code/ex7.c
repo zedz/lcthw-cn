2ec0cc95b4bb08e27a986d3f73bd1a7b8a08266f
Ian Murray <ian.c.murray@gmail.com>
2011-09-28 22:27:13 +0100

    Change integer literals to long literals.
    This is important when increasing the value of 'universe_of_defects'
    to cause an overflow, as otherwise the overflow occurs in the expression
    on the right-hand-side.

--- code/ex7.c
+++ code/ex7.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     printf("You have %d bugs imaginary rate of %f.\n",
             bugs, bug_rate);
 
-    long universe_of_defects = 1 * 1024 * 1024 * 1024;
+    long universe_of_defects = 1L * 1024L * 1024L * 1024L;
     printf("The entire universe has %ld bugs.\n",
             universe_of_defects);
 
