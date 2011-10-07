12850e3eded72b170afb45309a8eb16c392999cc
Zed A. Shaw <zedshaw@zedshaw.com>
2011-10-07 10:39:44 -0700

    Some cleanup and refining.

--- code/ex23.c
+++ code/ex23.c
@@ -20,15 +20,15 @@ int duffs_device(char *from, char *to, int count)
         int n = (count + 7) / 8;
 
         switch(count % 8) {
-            case 0:      do { *to++ = *from++;
-                             case 7: *to++ = *from++;
-                             case 6: *to++ = *from++;
-                             case 5: *to++ = *from++;
-                             case 4: *to++ = *from++;
-                             case 3: *to++ = *from++;
-                             case 2: *to++ = *from++;
-                             case 1: *to++ = *from++;
-                         } while(--n > 0);
+            case 0: do { *to++ = *from++;
+                        case 7: *to++ = *from++;
+                        case 6: *to++ = *from++;
+                        case 5: *to++ = *from++;
+                        case 4: *to++ = *from++;
+                        case 3: *to++ = *from++;
+                        case 2: *to++ = *from++;
+                        case 1: *to++ = *from++;
+                    } while(--n > 0);
         }
     }
 
@@ -41,17 +41,17 @@ int zeds_device(char *from, char *to, int count)
         int n = (count + 7) / 8;
 
         switch(count % 8) {
-             case 0: 
-                 again: *to++ = *from++;
+            case 0: 
+            again: *to++ = *from++;
 
-             case 7: *to++ = *from++;
-             case 6: *to++ = *from++;
-             case 5: *to++ = *from++;
-             case 4: *to++ = *from++;
-             case 3: *to++ = *from++;
-             case 2: *to++ = *from++;
-             case 1: *to++ = *from++;
-                     if(--n > 0) goto again;
+            case 7: *to++ = *from++;
+            case 6: *to++ = *from++;
+            case 5: *to++ = *from++;
+            case 4: *to++ = *from++;
+            case 3: *to++ = *from++;
+            case 2: *to++ = *from++;
+            case 1: *to++ = *from++;
+                    if(--n > 0) goto again;
         }
     }
 
