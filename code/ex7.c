0ceceeb31e3150d1e5392f1e74c751e9c75b7576
Martin Matusiak <numerodix@gmail.com>
2011-11-08 22:23:31 +0100

    fix typo in program output

--- code/ex7.c
+++ code/ex7.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
     int bugs = 100;
     double bug_rate = 1.2;
 
-    printf("You have %d bugs imaginary rate of %f.\n",
+    printf("You have %d bugs at the imaginary rate of %f.\n",
             bugs, bug_rate);
 
     long universe_of_defects = 1L * 1024L * 1024L * 1024L;
